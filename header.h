61a4f6252bea711c862c2e881f23b32541b06bdb
Atharva Shailesh Gadad <79466218+AthShaGad@users.noreply.github.com>
2023-04-03 11:09:17 +0530

    Update header.h

--- header.h
+++ header.h
@@ -3,6 +3,6 @@ void day_calc(int dd, int mm, int yyyy);
 void my_cal(int m, int y);
 int get_1st_weekday(int year);
 int func2();
-int shivadatta();
+int color();
 int sort();
 int monthly_ledger();
