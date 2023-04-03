56f1a2a474b49df1159e7a33d2bdf1b6ca445098
Atharva Shailesh Gadad <79466218+AthShaGad@users.noreply.github.com>
2023-04-03 11:10:08 +0530

    Update

--- make.mk
+++ make.mk
@@ -1,13 +1,13 @@
-a.out: planner.o prelim.o pw.o shivadatta.o ledger.o programsort.o client.o
-	gcc planner.o prelim.o pw.o shivadatta.o ledger.o programsort.o client.o
+a.out: planner.o prelim.o pw.o color.o ledger.o programsort.o client.o
+	gcc planner.o prelim.o pw.o color.o ledger.o programsort.o client.o
 planner.o: planner.c header.h
 	gcc -c planner.c
 prelim.o: prelim.c header.h
 	gcc -c prelim.c 
 pw.o: pw.c header.h
 	gcc -c pw.c
-shivadatta.o: shivadatta.c header.h
-	gcc -c shivadatta.c
+color.o: color.c header.h
+	gcc -c color.c
 ledger.o: ledger.c header.h
 	gcc -c ledger.c
 programsort.o: programsort.c header.h
